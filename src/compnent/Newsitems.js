4030551083cfa261461cbb0ac3804d02d5e8bb71
rushikbisen <rishikbisen@gmail.com>
2023-01-22 12:23:19 +0530

    abc

---
+++ src/compnent/Newsitems.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import News from './News'
+
+export class Newsitems extends Component {
+  render() {
+    let {title,description,imageurl,newsurl} =this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}....</p>
+            <a rel="nonreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Newsitems
